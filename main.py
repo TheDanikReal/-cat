5b7035108105932fb9631c1496d6af85f13aea17
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2022-07-01 21:15:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ import random
 numberrandom = random.randint(1, 5)
 mynumber = input(Угадай число от 1 до 5: )
 if numberrandom = mynumber:
- print("Ты угадал число!")
 else:
- print(f"Ты не угадал число /n Правильное число это: {numberrandom}")
 print("надеюсь тебе понравилось)
