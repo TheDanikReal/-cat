ad990d98cce433915bc6733d3b7aa51c1eae209f
TheDanikReal <99135042+TheDanikReal@users.noreply.github.com>
2022-07-02 09:55:51 +0300

    Fix some errors

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import random
 numberrandom = random.randint(1, 5)
 mynumber = input(Угадай число от 1 до 5: )
-if numberrandom = mynumber:
+if numberrandom == mynumber:
+  print("Ты угадал!")
 else:
+  print(f"Ты не угадал! \nравильное число было: {numberrandom}")
 print("надеюсь тебе понравилось)
